0623cf1ad0ed2614f4ca5ebcd934aa8baf017bfe
Oscar <oscar.enzing@gmail.com>
2019-06-14 12:43:21 +0200

    test

--- tests/test_flaskapp.py
+++ tests/test_flaskapp.py
@@ -1,5 +1,5 @@
 import unittest, json
-
+import base64
 from main import app
 
 
@@ -11,17 +11,19 @@ class FlaskTestCase(unittest.TestCase):
     def test_unauthorized(self):
         response = self.app.get('/authorized')
         # Check if the request fails with authorization error
-        self.assertEqual(response._status_code,401,'Unauthorized access to page without login')
+        self.assertEqual(response._status_code, 401, 'Unauthorized access to page without login')
 
     def test_authorisation(self):
-        response = self.app.get('/authorized')
-        resp = response.data.decode('utf-8')
-        self.assertEqual(resp, "You are logged in", 'login no work')
+        valid_credentials = base64.b64encode(b'travisci:tutorial').decode('utf-8')
+        response = self.app.get(
+            '/authorized', headers={'Authorization': 'Basic ' + valid_credentials})
+        assert response.status == '200 OK'
+        assert response.data.decode('utf-8') == "You are logged in"
 
     def test_multiply(self):
         response = self.app.get('/multiply?x=5&y=7')
         resp = json.loads(response.data)
-        self.assertEqual(resp['answer'],35,'Multiply endpoint failed known answer 7*5 = 35')
+        self.assertEqual(resp['answer'], 35, 'Multiply endpoint failed known answer 7*5 = 35')
 
     def test_hello_world(self):
         response = self.app.get('/')
